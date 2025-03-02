1009885174bf6df7ce255b4239576af72dcf3e8c
ssd <2537916417@qq.com>
2025-03-02 16:02:47 +0800

    删除多个不再使用的文件，包括可执行文件、Python 脚本和数据库相关的 JavaScript 文件

--- my_script.py
+++
@@ -1,2 +0,0 @@
-# my_script.py
-print("Hello from Python! 这个脚本是通过 Node.js 触发的。")
--- ttst.py
+++
@@ -1,7 +0,0 @@
-# from openai import OpenAI
-# print("导入成功")
-import sys
-import os
-
-print("Python解释器路径:", sys.executable)
-print("模块搜索路径:", sys.path)
